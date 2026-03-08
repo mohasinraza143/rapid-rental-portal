cd76b42db6fd990a9d8781e7fa1c0d1228c63907
Mohasin Raza <112263298+mohasinraza143@users.noreply.github.com>
2026-03-08 15:13:00 +0530

    Add files via upload

---
+++ contact-us.php
@@ -0,0 +1,168 @@
+ <?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+<title>Car Rental|| Contact Us Page</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<!--Custome Style -->
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<!--OWL Carousel slider-->
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<!--slick-slider -->
+<link href="assets/css/slick.css" rel="stylesheet">
+<!--bootstrap-slider -->
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<!--FontAwesome Font Style -->
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+
+ <style>
+    .errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+    </style>
+</head>
+<body>
+     
+<!--Header-->
+<?php include('includes/header.php');?>
+<!-- /Header --> 
+
+<!--Page Header-->
+<section class="page-header contactus_page">
+  <div class="container">
+    <div class="page-header_wrap">
+      <div class="page-heading">
+        <h1>Contact Us</h1>
+      </div>
+      <ul class="coustom-breadcrumb">
+        <li><a href="#">Home</a></li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  </div>
+  <!-- Dark Overlay-->
+  <div class="dark-overlay"></div>
+</section>
+
+
+<!--Contact-us-->
+<section class="contact_us section-padding">
+  <div class="container">
+    <div  class="row">
+      <div class="col-md-6">
+        <h3>Get in touch using the form below</h3>
+        <div class="contact_form gray-bg">
+          <form >
+            <div class="form-group">
+              <label class="control-label">Full Name <span>*</span></label>
+              <input type="text" name="fullname" class="form-control white_bg" id="fullname" required>
+            </div>
+            <div class="form-group">
+              <label class="control-label">Email Address <span>*</span></label>
+              <input type="email" name="email" class="form-control white_bg" id="emailaddress" required>
+            </div>
+            <div class="form-group">
+              <label class="control-label">Phone Number <span>*</span></label>
+              <input type="text" name="contactno" class="form-control white_bg" id="phonenumber" required maxlength="10" pattern="[0-9]+">
+            </div>
+            <div class="form-group">
+              <label class="control-label">Message <span>*</span></label>
+              <textarea class="form-control white_bg" name="message" rows="4" required></textarea>
+            </div>
+            <div class="form-group">
+              <button class="btn" type="submit" name="send" type="submit">Send Message <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <!-- Contact info -->
+
+      <div class="col-md-6">
+        <h3>Contact Info</h3>
+        <div class="contact_detail">
+          <ul>
+            <li>
+              <div class="icon_wrap"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+              <div class="contact_info_m">NH-65,Balasore,Odisha</div>
+            </li>
+            <li>
+              <div class="icon_wrap"><i class="fa fa-phone" aria-hidden="true"></i></div>
+              <div class="contact_info_m">9437912800
+             <li>
+              <div class="icon_wrap"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
+              <div class="contact_info_m">mohsin@gmail.com</div>
+            </li>
+          </ul>
+        <?php  ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- /Contact-us--> 
+
+
+<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer--> 
+
+
+
+<!--Login-Form -->
+<?php include('includes/login.php');?>
+<!--/Login-Form --> 
+
+<!--Register-Form -->
+<?php include('includes/registration.php');?>
+
+
+
+ 
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+
+</html>
